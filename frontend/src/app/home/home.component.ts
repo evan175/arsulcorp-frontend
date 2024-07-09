4fd279028c9e198acb2fb0b604c53d70b7c6031b
Ashraful Hoque <ahoque@umass.edu>
2024-07-09 18:38:50 -0400

    home page skeleton

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarRow } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatToolbarRow, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
