427c625aeb1d8c804b5694b888238633e5e462bf
Ashraful Hoque <ahoque@umass.edu>
2024-07-24 16:47:45 -0400

    added footer

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-62X2DRTF.mjs";import"./chunk-3IWMMDHG.mjs";import"./chunk-HTUQMP4E.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-P5S4D5RO.mjs";import"./chunk-3IWMMDHG.mjs";import"./chunk-HTUQMP4E.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from "./home/home.component";
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent, RouterLink, RouterLinkActive, HeaderComponent]
+    imports: [RouterOutlet, HomeComponent, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'frontend';
