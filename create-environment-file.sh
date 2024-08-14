730fb263073e2cba3b7b5b46808a5295c4a0b483
Ashraful Hoque <ahoque@umass.edu>
2024-08-13 23:08:50 -0400

    fixed env building script

--- create-environment-file.sh
+++ create-environment-file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the environments directory if it doesn't exist
-mkdir -p src/environments
+mkdir -p frontend/src/environments
 
 # Write the environment.ts file
-cat <<EOT > src/environments/environment.ts
+cat <<EOT > frontend/src/environments/environment.ts
 export const environment = {
   apiUrl: '$apiUrl',
   cognito: JSON.parse('$cognito')
