88b941ac4348ed96049875d6964ab8a3d3ab03aa
Ashraful Hoque <ahoque@umass.edu>
2024-09-03 02:22:23 -0400

    added image carousel

---
+++ frontend/src/app/featured/featured.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-featured',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule],
+  templateUrl: './featured.component.html',
+  styleUrl: './featured.component.css'
+})
+export class FeaturedComponent {
+
+}
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,11 +4,14 @@ import { MatToolbarRow } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { FeaturedComponent } from '../featured/featured.component';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatToolbarRow, MatButtonModule, RouterOutlet, RouterLink],
+  imports: [MatToolbarModule, MatToolbarRow, MatButtonModule, RouterOutlet, RouterLink, FeaturedComponent, MatDividerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
