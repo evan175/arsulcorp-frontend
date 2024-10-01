0c949c284213eadc662e315ab7f363233c537656
Ashraful Hoque <ahoque@umass.edu>
2024-10-01 04:20:03 -0400

    added s3url to create env file

--- create-environment-file.sh
+++ create-environment-file.sh
@@ -7,6 +7,7 @@ mkdir -p frontend/src/environments
 cat <<EOT > frontend/src/environments/environment.ts
 export const environment = {
   apiUrl: '$apiUrl',
-  cognito: JSON.parse('$cognito')
+  cognito: JSON.parse('$cognito'),
+  s3Url: '$s3Url'
 };
 EOT
