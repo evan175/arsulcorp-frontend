0f6a33667727360a19324c3e9b1e0eb6022188ca
Ashraful Hoque <ahoque@umass.edu>
2025-03-21 14:44:41 -0400

    added test apiurl

--- create-environment-file.sh
+++ create-environment-file.sh
@@ -5,6 +5,7 @@ mkdir -p frontend/src/environments
 cat <<EOT > frontend/src/environments/environment.ts
 export const environment = {
   apiUrl: '$apiUrl',
+  testApiUrl: '$testApiUrl',
   cognito: JSON.parse('$cognito'),
   s3Url: '$s3Url'
 };
